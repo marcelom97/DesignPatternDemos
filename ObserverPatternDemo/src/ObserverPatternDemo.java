2111e297d3b4a2a1573175059e0e9570589a77ba
marcelom97 <marcelomollaj@gmail.com>
2020-09-25 22:31:03 +0300

    [added] changed Observer main class

--- ObserverPatternDemo/src/ObserverPatternDemo.java
+++ ObserverPatternDemo/src/ObserverPatternDemo.java
@@ -3,8 +3,8 @@ public class ObserverPatternDemo {
 
         Subject subject = new Subject(1);
         Observer observer = new BinaryObserver(subject);
-        subject.setState(2);
+        subject.setState(2); // with one observer
         Observer observer1 = new BinaryObserver(subject);
-        subject.setState(3);
+        subject.setState(3); // with two observers
     }
 }
